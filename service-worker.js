8658ad529b441f8157d55292c003454fdf801755
Dr. Frederick Vaz <frederickvaz@rediffmail.com>
2025-04-15 11:06:31 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,26 @@
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("app-cache").then((cache) => {
-            return cache.addAll(["index.html", "app.js", "manifest.json"]);
-        })
-    );
+  event.waitUntil(
+    caches.open("srq-cache-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "app.js",
+        "manifest.json",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        return new Response("Offline: Resource not available.", {
+          headers: { "Content-Type": "text/plain" }
+        });
+      });
+    })
+  );
 });
